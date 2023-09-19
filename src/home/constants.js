c0778784f3483522ad283d1ce9a6149dfc7c2858
Martin Sugasti <martinsugasti@gmail.com>
2023-09-19 14:04:33 +0200

    Update Google Recaptcha site key

--- src/home/constants.js
+++ src/home/constants.js
@@ -1,4 +1,4 @@
-export const GOOGLE_RECAPTCHA_SITE_KEY = '6LcC3E0mAAAAAIvMT37as-wcAnbBjSnnq59tWod4';
+export const GOOGLE_RECAPTCHA_SITE_KEY = '6LfFLTkoAAAAAIgME2HzApeaGhM21uK3DFQ653yS';
 
 export const EMAIL_JS_SERVICE_ID = 'service_6sti0yl';
 export const EMAIL_JS_TEMPLATES_ID = { contact: 'template_9wsbkf4', quoter: 'template_rzdg7qu' };
