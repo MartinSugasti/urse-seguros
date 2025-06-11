f58417d62c39ff3f4cd23141fd3802840f41c932
Martin Sugasti <martinsugasti@gmail.com>
2025-06-11 19:51:26 -0300

    General changes (#14)

--- src/app/sections/about/components/NuestraHistoria.jsx
+++ src/app/sections/about/components/NuestraHistoria.jsx
@@ -4,12 +4,19 @@ const NuestraHistoria = () => (
       <div className="text-center text-light">
         <h1>Nuestra Historia</h1>
         <div className="mt-4 px-3 px-sm-5 fst-italic">
-          <p>En Abril de 1987 fuimos designado Productor de Seguros por parte del Banco de Seguros del Estado. Comenzamos a gestar nuestra propia cartera de clientes, pero a su vez, contamos con el gran apoyo del corredor Armando TUBINO, quién nos fue transfiriendo la misma para atender sus clientes de acuerdo a los tiempos que por esos años 90 la gente necesitaba, apoyados en tecnología y calidad de servicio. En el año 1995 se adquiere el local donde hoy se encuentra nuestra oficina; Ursino Barreiro 3452 casi Independencia, frente a la Sucursal Florida del Banco de Seguros del Estado, siendo el primer Corredor de Seguros instalado en forma profesional en el Departamento. Por estos tiempos que mencionamos, se desmonopoliza la cartera de Seguros de Automóviles, comenzando la competencia en dicho ramo. Iniciamos actividades con compañías privadas , siendo necesaria la inversión en capacitación del personal y en  mejor tecnología para adecuarnos a los nuevos desafíos que por esos años imperaba.</p>
-          <p>Nos asociamos a APROASE Y CUOAPROSE, agremiaciones que nuclea a los Productores-Asesores de Seguros del Uruguay para contar con información necesaria de la propia actividad.</p>
-          <p>Ingresamos a CASUR, primera Cooperativa de Corredores de Seguros del Uruguay, integrada por sesenta corredores, y cuya agrupación es Broker de varias compañías de seguros que operan en el medio.</p>
-          <p>En Agosto de 2009, entra en vigencia la Ley 18412, donde es Obligatorio asegurar los vehículos, por lo que el crecimiento de nuestra cartera, nos obligó a contratar más personal y readecuar las instalaciones, con el fin de realizar una correcta atención de nuestros clientes y mejorar la  comodidad de nuestro equipo de trabajo.</p>
-          <p>Actualmente trabajamos para todas las compañías de seguros que operan en plaza y somos Broker de la compañía SANCOR.</p>
-          <p>Hoy, luego de más de 30 años de actividad, la cual requiere de mucha actitud, perseverancia, extremada responsabilidad y trabajo, somos referentes en materia de seguros y estamos dentro de los corredores más importantes del Banco de Seguros de Estado del interior del país.</p>
+          <p>En abril de 1987 fuimos designado Productor de Seguros por parte del Banco de Seguros del Estado. Comenzamos a gestar nuestra propia cartera de clientes, pero a su vez, contamos con el gran apoyo del corredor Armando Tubino, quien nos fue transfiriendo la misma para atender sus clientes de acuerdo a los tiempos que por esos años 90, la gente necesitaba, apoyados en tecnología y calidad de servicio. En el año 1995 se adquiere el local donde hoy se encuentra nuestra oficina, Ursino Barreiro 3452, casi Independencia, frente a la Sucursal Florida del Banco de Seguros del Estado, siendo el primer Corredor de Seguros instalado en forma profesional en el Departamento. Por estos tiempos que mencionamos se desmonopoliza la cartera de Seguros de Automoviles, comenzando la competencia con compañías privadas, siendo necesaria la inversión en capacitación del personal y en mejor tecnología para adecuarnos a los nuevos desafíos que por esos años imperaba.</p>
+
+          <p>Nos asociamos a APROASE Y CUAPROSE, agremiaciones que nuclea a los Productos - Asesores de Seguros del Uruguay para contar con información necesaria de la propia actividad. <strong>Actualmente, y con mucho orgullo presidimos CUAPROSE.</strong></p>
+
+          <p>Ingresamos a CASUR, primera Cooperativa de Corredores de Seguros del Uruguay, integrada por ochenta corredores y cuya agrupación es Broker de varias compañías de seguros que operan en el medio. <strong>En la misma fuimos Presidente en el periodo 2012 - 2014.</strong></p>
+
+          <p>En agosto de 2009, entra en vigencia la Ley 18.412, donde es Obligatorio asegurar los vehículos , por lo que el crecimiento de nuestra cartera, nos obligo a contratar mas personal y readecuar las instalaciones con el fin de realizar una correcta atención de nuestros clientes y mejorar la comodidad de nuestro equipo de trabajo.</p>
+
+          <p>Actualmente trabajamos para todas las compañías de seguros que operan en plaza y somos Broker de la compañía SANCOR desde hace 18 años.</p>
+
+          <p>Contamos con un excelente equipo de trabajo de ocho personas consolidado, con mas de diez años de cada uno de ellos. Pasamos por el proceso de Certificacion en Calidad que nos ayudo a mejorar procedimientos y formas de trabajo.</p>
+
+          <p>Hoy luego de mas de 35 años de actividad , la cual requiere de mucha actitud, perseverancia, extremada responsabilidad y trabajo, somos referentes en materia de seguros y estamos dentro de los corredores mas importante del Banco de Seguros del Estado del interior del país.</p>
         </div>
       </div>
     </div>
